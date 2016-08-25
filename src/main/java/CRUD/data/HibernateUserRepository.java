1c8ea02401029feb9fc2dd000c0ef11c008649a7
Sanzhar <sanjar.satybaldiyev@gmail.com>
2016-08-25 20:50:24 +0500

    no message

--- src/main/java/CRUD/data/HibernateUserRepository.java
+++ src/main/java/CRUD/data/HibernateUserRepository.java
@@ -5,18 +5,12 @@ import CRUD.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 @Repository
 public class HibernateUserRepository implements UserRepository {
-    private static final Logger logger = LoggerFactory.getLogger(HibernateUserRepository.class);
 
     private SessionFactory sessionFactory;
     private static final int limitResultsPerPage = 5;
@@ -29,14 +23,12 @@ public class HibernateUserRepository implements UserRepository {
     public void addUser(User user) {
         Session session = sessionFactory.getCurrentSession();
         session.persist(user);
-        logger.info("User successfully added. User info: " + user);
     }
 
     @Override
     public void updateUser(User user) {
         Session session = sessionFactory.getCurrentSession();
         session.update(user);
-        logger.info("User info successfully updated. Updated user info: " + user);
     }
 
     @Override
@@ -45,14 +37,12 @@ public class HibernateUserRepository implements UserRepository {
         User user = (User) session.get(User.class, id);
         if (user != null)
             session.delete(user);
-        logger.info("User was successfully deleted. User info: " + user);
     }
 
     @Override
     public User getUser(int id) {
         Session session = sessionFactory.getCurrentSession();
         User user = (User) session.get(User.class, id);
-        logger.info("User successfully loaded. User info: " + user);
         return user;
     }
 
@@ -64,9 +54,6 @@ public class HibernateUserRepository implements UserRepository {
         query.setFirstResult((int)(page - 1) * limitResultsPerPage);
         query.setMaxResults(limitResultsPerPage);
         List<User> users = query.list();
-        for (User curr : users) {
-            logger.info("User list info: " + curr);
-        }
         return users;
     }
 
@@ -77,9 +64,6 @@ public class HibernateUserRepository implements UserRepository {
         Query query = session.createQuery("FROM User WHERE NAME = :name");
         query.setParameter("name", name);
         List<User> users = query.list();
-        for (User curr : users) {
-            logger.info("User list info: " + curr);
-        }
         return users;
     }
 
@@ -88,9 +72,6 @@ public class HibernateUserRepository implements UserRepository {
     public List<User> getAdmins() {
         Session session = sessionFactory.getCurrentSession();
         List<User> users = session.createQuery("FROM User WHERE ISADMIN = 1").list();
-        for (User curr : users) {
-            logger.info("User list info: " + curr);
-        }
         return users;
     }
 }
