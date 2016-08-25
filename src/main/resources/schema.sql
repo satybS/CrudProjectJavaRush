d50aca0d8e78079acea5280a7f5627e6964fb0da
Sanzhar <sanjar.satybaldiyev@gmail.com>
2016-08-25 21:06:01 +0500

    no message

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table Spittle (
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	message varchar(140) not null,
-	created_at timestamp not null,
-	latitude double,
-	longitude double, 
-    PRIMARY KEY (id)
-);
